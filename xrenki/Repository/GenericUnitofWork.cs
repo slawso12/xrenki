aac094ea606abecc0178ed2b541b0ffe58dd1a3e
Stephon Lawson <slawso12@gmail.com>
2020-06-01 15:46:05 -0700

    Update: Repository Pattern Implemented

---
+++ xrenki/Repository/GenericUnitofWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using xrenki.DAL;
+
+namespace xrenki.Repository
+{
+    public class GenericUnitofWork:IDisposable
+    {
+        private dbHBAEStoreEntities DBEntity = new dbHBAEStoreEntities();
+        public IRepository<Tbl_EntityType> GetRepositoryInstance<Tbl_EntityType>() where Tbl_EntityType : class
+        {
+            return new GenericRepository<Tbl_EntityType>(DBEntity);
+        }
+
+        public void SaveChanges()
+        {
+            DBEntity.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    DBEntity.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool disposed = false;
+    }
+}
